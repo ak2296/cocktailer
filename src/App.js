31b5a10ac0c822f681486eace69d43fac74c6e0e
ak2296 <elohin_m@yahoo.com>
2021-05-29 08:12:49 +0200

    attempt2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import Axios from 'axios';
 import { useState } from 'react';
 import Tiles from "./Tiles"
 
+
 function App() {
+  
   const[query, setquery]=useState("")
   const [drinks, setdrinks] = useState([])
-  var url=`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`
+  const [searchType, setsearchType] = useState('search.php?s')
+  var url= `https://www.thecocktaildb.com/api/json/v1/1/${searchType}=${query}`
   async function getDrinks(){
     let result= await Axios.get(url);
     setdrinks(result.data.drinks);
@@ -20,25 +23,34 @@ function App() {
   return (
     <div className='container'>
       <div className='row justify-content-md-center py-3'>
-      <h2 className='col text-center' onClick={getDrinks}>Cocktailer</h2>
+      <h2 className='col text-center'>Cocktailer</h2>
       </div>
       <div className="row  justify-content-center">
-        <div className="col-lg-6">
+        <div className="col-lg-8">
         <form className='input-group' onSubmit={submit}>
           <input 
           type="text" className="form-control mb-3" 
           placeholder="Search for a drink..." 
           value={query} onChange={(e)=> setquery(e.target.value)}/>
           <button type="submit" className="btn btn-primary mb-3">search</button>
+          <div className="col-2 ml-5">
+          <select className="form-select text-truncate">
+            <option  onclick={()=>setsearchType('search.php?s')}> Name</option>
+            <option  onSelect={()=>setsearchType('filter.php?i')}> Ingredients</option>
+          </select>
+          </div>
         </form>
         </div>
         </div>
-      <div>
+        <div className="container ">
+        <div className="row  justify-content-center">
         {drinks.map(drink => {
           return <Tiles drink= {drink} />;
         })}
+        </div>
+        </div>
       </div>
-    </div>
+    
     
 )}
 
--- src/Tiles.js
+++ src/Tiles.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 export default function Tiles({drink}) {
     return (
-        <div className=" container">
-            <img src={drink["strDrinkThumb"]+"/preview"}/>
-            <p>{drink["strDrink"]}</p>
-        </div>
+        
+            <div className="col-sm-6 col-md-auto mb-3 text-center " onClick={()=>{
+                console.log('hi')
+            }}>
+            <img src={drink["strDrinkThumb"]+"/preview"} className="fluid img-thumbnail mt-3" alt="cocktails"/>
+            <p className="fw-bold">{drink["strDrink"]}</p>
+            </div>
+        
     )
 }
