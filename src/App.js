2fe037314768e756661e8b80c86b938c09046ca6
ak2296 <elohin_m@yahoo.com>
2021-05-29 19:08:49 +0200

    2nd page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import Axios from 'axios';
 import { useState } from 'react';
 import Tiles from "./Tiles"
-
+import Drink from "./Drink"
 
 function App() {
   
   const[query, setquery]=useState("")
   const [drinks, setdrinks] = useState([])
   const [searchType, setsearchType] = useState('search.php?s')
+
   var url= `https://www.thecocktaildb.com/api/json/v1/1/${searchType}=${query}`
   async function getDrinks(){
     let result= await Axios.get(url);
     setdrinks(result.data.drinks);
-    console.log(result.data);
+    //console.log(result.data);
   }
 
   const submit = (e)=>{
@@ -35,8 +36,8 @@ function App() {
           <button type="submit" className="btn btn-primary mb-3">search</button>
           <div className="col-2 ml-5">
           <select className="form-select text-truncate">
-            <option  onclick={()=>setsearchType('search.php?s')}> Name</option>
-            <option  onSelect={()=>setsearchType('filter.php?i')}> Ingredients</option>
+            <option  value="Name" onClick={()=>setsearchType('search.php?s')}> Name</option>
+            <option  value="Ingredients" onSelect={()=>setsearchType('filter.php?i')}> Ingredients</option>
           </select>
           </div>
         </form>
--- src/Tiles.js
+++ src/Tiles.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Drink from './Drink'
 
 export default function Tiles({drink}) {
     return (
         
             <div className="col-sm-6 col-md-auto mb-3 text-center " onClick={()=>{
-                console.log('hi')
+                let drinkId = drink["idDrink"]
+                Drink(drinkId)
             }}>
             <img src={drink["strDrinkThumb"]+"/preview"} className="fluid img-thumbnail mt-3" alt="cocktails"/>
             <p className="fw-bold">{drink["strDrink"]}</p>
