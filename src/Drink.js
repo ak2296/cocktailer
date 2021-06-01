85efc60d3ca60a280fc89355917c26432e4382d5
ak2296 <62262817+ak2296@users.noreply.github.com>
2021-06-01 17:31:50 +0200

    page shown

--- src/Drink.js
+++ src/Drink.js
@@ -4,32 +4,33 @@ import Axios from 'axios';
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id')
+if(id!==null){ 
+  Drink()
+}
 
+function Drink() {
 
+  var details=[]
+  let descurl = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='+id
 
-export default function Drink() {
-
-
-        let details=[]
-
-        let descurl = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='+id
         async function getDescrip(){
             let descrip= await Axios.get(descurl);
-            details= descrip.data.drinks['0'];
-            console.log(details);
+            details.push(descrip.data.drinks['0']) ;
+            
         }
-        if(id!=null){ 
-        getDescrip();}
-
+        if(id){ 
+          getDescrip();
+        }
+    console.log(details[0])
         return (
+
            <div className="col-sm-6 col-md-auto mb-3 text-center ">
                 <h1>this is has to show on page!</h1>
-
-                <h1 className="fw-bold">{details["strDrink"]}</h1>
-                <img src={details["strDrinkThumb"]} className="fluid img-thumbnail mt-3" alt="cocktails"/>
-    
-    
+                <h1 >{details["strDrink"]}</h1>
+                <img src={details["strDrinkThumb"]} className="img-thumbnail mt-3" alt="cocktails"/>
           </div>
         )
 
 }
+
+export default Drink;
