32c1688d91d562e0b0e45035f5821f64d974cc84
ak2296 <62262817+ak2296@users.noreply.github.com>
2021-06-01 21:17:55 +0200

    1/6

--- src/Drink.js
+++ src/Drink.js
@@ -5,25 +5,25 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id')
 
-var ing=[];
+var ingred=[];
 
 function Drink() {
 
   let url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='+id
   async function getDescrip(){
     let result= await Axios.get(url);
-    if(ing.length<1){
-    ing.push(result.data.drinks['0']);
+    if(ingred.length<1){
+    ingred.push(result.data.drinks['0']);
     }
   }
     getDescrip();
-    console.log(ing)
+    console.log(ingred)
 
     return (
        <div className="col-sm-6 col-md-auto mb-3 text-center ">
             <h1>this is has to show on page!</h1>
-             <h1 >{ing["strDrink"]}</h1>
-             <img src={ing["strDrinkThumb"]} className="img-thumbnail mt-3" alt="cocktails"/>
+             <h1 >{ingred["strDrink"]}</h1>
+             <img src={ingred["strDrinkThumb"]} className="img-thumbnail mt-3" alt="cocktails"/>
       </div>
      )
      
