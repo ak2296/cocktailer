77e850c5a1a1d0ebafd6bf5865b82bdcb2261a3d
ak2296 <elohin_m@yahoo.com>
2021-06-06 13:48:23 +0200

    Add to favorite added

--- src/Favorites.js
+++ src/Favorites.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Tiles from "./Tiles";
 
-export default function Favorites({Ingred}) {
-    console.log(Ingred)
-    console.log('hi')
-    //here we have to have a picture and the name of drink just like tiles that links to its drink page
-    return (
-        <div className="col  mb-3 text-center " >
-        <h3 className="col-3 border-bottom border-warning pb-3 m-auto ">My favorites</h3>
-        <div className="">
-            here comes the tiles
+export default function Favorites({ Ingred }) {
+  const [favs, setFavs] = useState(
+    JSON.parse(localStorage.getItem("favoriteDrinks"))
+  );
+
+  useEffect(() => {
+    setFavs(JSON.parse(localStorage.getItem("favoriteDrinks")));
+  }, []);
+
+  //here we have to have a picture and the name of drink just like tiles that links to its drink page
+  return (
+    <div className="col  mb-3 text-center ">
+      <div>
+          <a href="/" target="_self">
+            <button type="button" className="btn btn-warning float-start ">
+              {" "}
+              Home
+            </button>
+          </a>
         </div>
+      <h3 className="col-3 border-bottom border-warning pb-3 m-auto ">
+        My favorites
+      </h3>
+      <div className="row justify-content-center">
+        <div className="col">
+        {favs.length === 0 ? <h4>No favorites</h4> : ""}
+        {favs.map((drink) => {
+          return <Tiles key={drink["idDrink"]} drink={drink} />;
+        })}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,66 +1,90 @@
-import React from 'react';
-import Axios from 'axios';
-import Tiles from "./Tiles"
-import  { useState } from 'react';
-
+import React from "react";
+import Axios from "axios";
+import Tiles from "./Tiles";
+import { useState } from "react";
 
 export default function Form() {
-  const[query, setquery]=useState("")
-  const [drinks, setdrinks] = useState([])
-  const [searchType, setsearchType] = useState('')
+  const [query, setquery] = useState("");
+  const [drinks, setdrinks] = useState([]);
+  const [searchType, setsearchType] = useState("search.php?s");
 
-  var url= `https://www.thecocktaildb.com/api/json/v1/1/${searchType}=${query}`
+  var url = `https://www.thecocktaildb.com/api/json/v1/1/${searchType}=${query}`;
   // Fetches API based on type of search and the value of search bar
-  async function getDrinks(){
-    let result= await Axios.get(url);
+  async function getDrinks() {
+    let result = await Axios.get(url);
     setdrinks(result.data.drinks);
   }
 
-  const submit = (e)=>{
+  const submit = (e) => {
     e.preventDefault();
     getDrinks();
-  }
-   
-    return (
-      // this is the search bar and buttons in search bar
-      // att the bottom it sends needed data to Tiles
-        <div className="row text-center ">  
-        <form className='row  justify-content-center ' onSubmit={submit}>
+  };
+
+  return (
+    // this is the search bar and buttons in search bar
+    // att the bottom it sends needed data to Tiles
+    <div className="row text-center ">
+      <form className="row  justify-content-center " onSubmit={submit}>
         <div className="col-10 col-md-6 ">
-            <input
-            type="text" className="form-control mb-3" 
-            placeholder="Search for a drink..." 
-            value={query} onChange={(e)=> setquery(e.target.value)}/>
+          <input
+            type="text"
+            className="form-control mb-3"
+            placeholder="Search for a drink..."
+            value={query}
+            onChange={(e) => setquery(e.target.value)}
+          />
         </div>
         <div className="col-10 col-md-2 text-center">
-            <button type="submit" className="container-fluid btn btn-secondary mb-3 ">Search</button>
+          <button
+            type="submit"
+            className="container-fluid btn btn-secondary mb-3 "
+          >
+            Search
+          </button>
         </div>
-          <div className="col-10 col-md-2">
+        <div className="col-10 col-md-2">
           <div className="form-check ">
-            <input className="form-check-input " type="radio" name="flexRadioDefault" id="flexRadioDefault1" onClick={()=>setsearchType('search.php?s')}/>
-            <label className="form-check-label " htmlFor="flexRadioDefault1">
+            <input
+              className="form-check-input"
+              checked
+              type="radio"
+              name="flexRadioDefault"
+              id="flexRadioDefault1"
+              onClick={() => setsearchType("search.php?s")}
+            />
+            <label className="form-check-label" htmlFor="flexRadioDefault1">
               Name
             </label>
-            </div>
+          </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"  onClick={()=>setsearchType('filter.php?i')}/>
+            <input
+              className="form-check-input"
+              type="radio"
+              name="flexRadioDefault"
+              id="flexRadioDefault2"
+              onClick={() => setsearchType("filter.php?i")}
+            />
             <label className="form-check-label " htmlFor="flexRadioDefault2">
               Ingredients
             </label>
-            </div>
+          </div>
         </div>
-    </form>
+      </form>
 
-       <div className="container">
-       <div className="row  justify-content-center">
-         {drinks ? (drinks.map(drink => {
-           return <Tiles key={drink['idDrink']} drink= {drink} />;
-         })):(<div className=" col-6 justify-content-center bg-danger py-2 text-center text-light mt-5  rounded-3"><h4 >No Drinks Found</h4>
-         <p>To search by ingredients enter the full name of it</p>
-         </div>)}
-       </div>
-       </div>
-       </div>
-    )
-
+      <div className="container">
+        <div className="row  justify-content-center">
+          {drinks ? (
+            drinks.map((drink) => {
+              return <Tiles key={drink["idDrink"]} drink={drink} />;
+            })
+          ) : (
+            <div className=" col-6 justify-content-center bg-danger py-2 text-center text-light mt-5  rounded-3">
+              <h4>No Drinks Found</h4>
+              <p>To search by ingredients enter the full name of it</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
